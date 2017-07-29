1d3a68c7ee3f5353bae9f0621b2657cf6eef2545
Andrew Blevins <ablevins@Andrews-MacBook-Pro.local>
2017-07-29 12:25:01 -0700

    floating text

---
+++ ReadyPowerOne/Assets/UI/FloatingText.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FloatingTextBehavior : MonoBehaviour {
+
+	public Animator animator;
+	private Text animatorText{ get; set;}
+
+	void Start()
+	{
+		AnimatorClipInfo[] clipInfo = animator.GetCurrentAnimatorClipInfo (0);
+		Destroy (gameObject, clipInfo [0].clip.length);
+		animatorText = animator.GetComponent<Text> ();
+	}
+
+}
---
+++ ReadyPowerOne/Assets/UI/TextFloatBehavior.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextFloatBehavior : MonoBehaviour {
+
+	private IEnumerator KillOnAnimationEnd() {
+		yield return new WaitForSeconds (.9f);
+		Destroy (gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		StartCoroutine (KillOnAnimationEnd ());
+	}
+}
