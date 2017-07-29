25629a3430a75b151aac201d4ae021eee6952537
4616 <dougal.michael@gmail.com>
2017-07-28 22:06:34 -0700

    capital eney

--- Ready Power One/Assets/Enemy.cs
+++ Ready Power One/Assets/Enemy.cs
@@ -2,15 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class enemy : MonoBehaviour {
+public class Enemy : MonoBehaviour {
+	public float health = 10f;
+	public float moveSpeed = 0.1f;
+	public float damage = 1f;
+	public float range = 1.5f;
 
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
 	}
+
+	void FindPlayer () {
+		Player.getPos
+
+	}
+
 }
