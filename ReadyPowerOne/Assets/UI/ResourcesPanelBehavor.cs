e74851d104dbe0ac5badac8bd4b969de09942651
Andrew Blevins <ablevins@Andrews-MacBook-Pro.local>
2017-07-29 17:00:10 -0700

    ui energy tracking

---
+++ ReadyPowerOne/Assets/UI/ResourcesPanelBehavor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourcesPanelBehavor : MonoBehaviour {
+
+	public Text voltsText;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void updateVolts(float t){
+		Debug.Log ("update to " + t);
+		voltsText.text = t.ToString();
+		Debug.Log ("updated " + voltsText.text);
+	}
+
+}
