5132a1c7b65d3e278928c1aea9701438cd2b5314
Andrew Blevins <ablevins@Andrews-MacBook-Pro.local>
2017-07-29 20:38:45 -0700

    aggregate popup text

--- ReadyPowerOne/Assets/UI/UIController.cs
+++ ReadyPowerOne/Assets/UI/UIController.cs
@@ -7,16 +7,15 @@ public class UIController : MonoBehaviour {
 
 	bool Paused;
 	public GameObject pausePanel;
-
-	public GameObject wallObject;
-
 	public ResourcesPanelBehavor resourcePanelPrefab;
-	public GameObject floatingText;
+	public GameObject floatingTextPrefab;
+
 
 	private ResourcesPanelBehavor resourcePanel;
 
-	private float lastDraw;
-
+	private float lastDraw = 0f;
+	private float DrawAmount = 0f;
+		
 	public static UIController Instance { get; private set; }
 
 	void Awake() {
@@ -30,35 +29,12 @@ public class UIController : MonoBehaviour {
 
 		// Here we save our singleton instance
 		Instance = this;
-
-		//Add terrain
-
-
 	
 	}
-
-	private float blockSize = 2.5f;
 
 	void Start () {
 		//Instantiate (pausePanel, this.transform);
 		resourcePanel = Instantiate (resourcePanelPrefab, this.transform);
-
-		Room room = TerrainGenerator.GenerateLevel (100, 100, 0);
-
-		for (int y = 0; y < room.getHeight(); y++) {
-			for (int x = 0; x < room.getWidth(); x++) {
-				Terrain t = room.getLayout () [y] [x];
-				if (t == Terrain.Wall) {
-					GameObject newWall = Instantiate (this.wallObject, this.transform);
-					newWall.transform.position = new Vector3 (x * blockSize, y * blockSize, 0);
-				}
-			}
-		}
-
-//		GameObject potato = Instantiate (this.wallObject, this.transform);
-//		potato.transform.position = new Vector3 (5, 5, 0);
-
-
 
 		//pausePanel.SetActive(false);
 	}
@@ -91,15 +67,20 @@ public class UIController : MonoBehaviour {
 	}
 
 	public void floatText(float floatnum,Transform location){
-		
-		GameObject ft = Instantiate (floatingText, this.transform);
+		DrawAmount += floatnum;
 
-		Vector3 drawlocation = Camera.main.WorldToScreenPoint (location.position);
-		Vector3 randvec = new Vector3 (Random.Range (-20, 20), 
-			                 Random.Range (-20, 20), 
-			                 0);
-		
-		ft.transform.position = drawlocation + randvec;
+		if  (Time.frameCount - lastDraw >= 12f) {
+			GameObject ft = Instantiate (floatingTextPrefab, this.transform);
+
+			Vector3 drawlocation = Camera.main.WorldToScreenPoint (location.position);
+			Vector3 randvec = new Vector3 (Random.Range (-20, 20), 
+				                  Random.Range (-20, 20), 
+				                  0);
+			ft.GetComponent<TextFloatBehavior>().floatText.text = DrawAmount.ToString();
+			ft.transform.position = drawlocation + randvec;
+			lastDraw = Time.frameCount;
+			DrawAmount = 0f;
+		} 
 
 	}
 
