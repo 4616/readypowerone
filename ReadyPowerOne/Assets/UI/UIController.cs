985627581446bbc06b5a15d34b355ab43ec03143
Andrew Blevins <ablevins@Andrews-MacBook-Pro.local>
2017-07-30 16:01:12 -0700

    working upgrades

--- ReadyPowerOne/Assets/UI/UIController.cs
+++ ReadyPowerOne/Assets/UI/UIController.cs
@@ -82,25 +82,24 @@ public class UIController : MonoBehaviour {
 
 	void Update() {
 		if (!Paused && Input.GetKeyDown(KeyCode.P) ) {
-			Pause();
+			openUpgradeMenu();
 		}
 		else if (Paused && Input.GetKeyDown(KeyCode.P) ) {
-			unPause();
+			closeUpgradeMenu();
 		}
 	}
 	
-	void Pause(){
-		upgradeMenu ();
+	void openUpgradeMenu(){
+		upgradePanel = Instantiate (upgradePanelPrefab, this.transform);
 		Paused = true;
-		//pausePanel.SetActive (true);
 
 		Time.timeScale = 0f;
 	}
 
-	void unPause(){
-		
+	public void closeUpgradeMenu(){
+		Debug.Log("close");
+		Destroy (upgradePanel.gameObject);
 		Paused = false;
-		//pausePanel.SetActive (false);
 
 		Time.timeScale = 1f;
 	}
@@ -134,11 +133,6 @@ public class UIController : MonoBehaviour {
 		} 
 
 	}
-
-	public void upgradeMenu(){
-		GameObject um = Instantiate (upgradePanelPrefab.gameObject, this.transform);
-
-		//um.GetComponent<UpgradePanelBehavior>().
-	}
+		
 
 }
